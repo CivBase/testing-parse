a16c67f254496cdac1404ebd8c1c9303eaadb80c
CivBase <blange@iastate.edu>
2015-08-27 13:59:03 -0500

    Improved the grunt build process and travis CI

--- Gruntfile.js
+++ Gruntfile.js
@@ -66,5 +66,5 @@ module.exports = function (grunt) {
  
   grunt.registerTask('build', ['typescript']);
   grunt.registerTask('lint', ['jslint', 'tslint']);
-  grunt.registerTask('default', ['lint', 'build']);
+  grunt.registerTask('default', ['tslint', 'build', 'jslint']);
 };
