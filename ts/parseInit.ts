b29e84fc77bcb3ff1bf5152123e88d2fa2c2a73a
CivBase <blange@iastate.edu>
2015-08-27 14:00:01 -0500

    Do not merge

--- ts/parseInit.ts
+++ ts/parseInit.ts
@@ -3,6 +3,7 @@ declare var Parse;
 (function (Parse) {
     'use strict';
 
+    // test comment
     Parse.initialize(
         'tdJFpgEza9WzemOR6nu37ATOl3iBIct2APklvOo7',
         'BRdphJDIoKHK0VAAWx9HjRckBKbGEwuW7PrQLEWO'
